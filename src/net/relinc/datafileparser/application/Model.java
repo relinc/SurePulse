0189b6f2e61bc8be46a8463f7510c17eb264abbf
Mark <halonen.mark@gmail.com>
2016-07-09 11:47:15 -0500

    #1 Automatic parsing parameters algorithm

--- src/net/relinc/datafileparser/application/Model.java
+++ src/net/relinc/datafileparser/application/Model.java
@@ -110,19 +110,147 @@ public class Model {
 	* 
 	*/
 	public boolean setParsingParametersAutomatically(){
+		resetParsingParameters();
+		
 		if(dataFile == null)
 			return false;
 		
 		String[] frameDelimiterCandidates = new String[]{"\n", System.lineSeparator()}; //TODO: Are there more?
-		String[] datapointDelimiterCandidates = new String[]{",", " ", "\t", "|", ":", "#", "$"}; //TODO: More?
+		String[] datapointDelimiterCandidates = new String[]{",", " ", "\t", "\\|", ":", "#", "$"}; //TODO: More?
 		
+		// Find the frame delimiter with the most data frames
 		String winningFrameDelimiter = frameDelimiterCandidates[0];
+		int winningFrameCount = 0;
 		String winningDatapointDelimiter = datapointDelimiterCandidates[0];
-		
+		int winningDatapointCount = 0;
+		String[] frames = new String[0];
 		for(int i = 0; i < frameDelimiterCandidates.length; i++){
-			
+			frames = dataFile.split(frameDelimiterCandidates[i]);
+			if(frames.length > winningFrameCount)
+			{
+				winningFrameCount = frames.length;
+				winningFrameDelimiter = frameDelimiterCandidates[i];
+			}
 		}
 		
+		if(winningFrameCount == 0)
+			return false;
+		
+		
+		// Find the datapoint delimiter with the most datapoints. Look at the middle data frame
+		String middleFrame = frames[frames.length / 2];
+		for(int i = 0; i < datapointDelimiterCandidates.length; i++){
+			String[] pts = middleFrame.split(datapointDelimiterCandidates[i]);
+			if(pts.length > winningDatapointCount){
+				winningDatapointCount = pts.length;
+				winningDatapointDelimiter = datapointDelimiterCandidates[i];
+			}
+		}
+		
+		if(winningDatapointCount == 0)
+			return false;
+		
+		if(winningDatapointDelimiter.equals(winningFrameDelimiter))
+			return false;
+		
+		// Now determine the start and end of the delimiters by trial and error numeric casting.
+		// Get the datapoint start/end from the middle frame, then work from top and bottom for the frames.
+		
+		String[] middleFramePts = middleFrame.split(getDatapointDelimiter());
+		boolean converts = false;
+		while(!converts){
+			converts = isDouble(middleFramePts[getStartDatapointDelimiter()]);
+			if(!converts)
+				setStartDatapointDelimiter(getStartDatapointDelimiter() + 1);
+			if(getStartDatapointDelimiter() >= middleFramePts.length)
+				return false;
+		}
+		
+		// Determine the end. Start with max and work backward.
+		setEndDatapointDelimiter(middleFramePts.length - 1);
+		converts = false;
+		while(!converts){
+			converts = isDouble(middleFramePts[getEndDatapointDelimiter()]);
+			if(!converts)
+				setEndDatapointDelimiter(getEndDatapointDelimiter() - 1);
+			if(getEndDatapointDelimiter() < 0) // Could be zero. One column
+				return false;
+		}
+		
+		// Set the frame begin. First row from the top where all start through end convert for datapoint delimiter.
+		converts = false;
+		while(!converts){
+			converts = true;
+			String[] pts = frames[getStartFrameDelimiter()].split(getDatapointDelimiter());
+			if(pts.length < getNumDatapoints()){
+				converts = false;
+			}
+			else{
+				for(int i = getStartDatapointDelimiter(); i <= getEndDatapointDelimiter(); i++){
+					if(!isDouble(pts[i])){
+						converts = false;
+					}
+				}
+			}
+			if(!converts)
+				setStartFrameDelimiter(getStartFrameDelimiter() + 1);
+			if(getStartFrameDelimiter() >= frames.length)
+				return false;
+		}
+		
+		
+		// Set the frame end. First row from the bottom where all start through end convert for datapoint delimiter.
+		converts = false;
+		setEndFrameDelimiter(frames.length - 1);
+		while(!converts){
+			converts = true;
+			String[] pts = frames[getEndFrameDelimiter()].split(getDatapointDelimiter());
+			if(pts.length < getNumDatapoints()){
+				converts = false;
+			}
+			else{
+				for(int i = getStartDatapointDelimiter(); i <= getEndDatapointDelimiter(); i++){
+					if(!isDouble(pts[i]))
+						converts = false;
+				}
+			}
+			if(!converts)
+				setEndFrameDelimiter(getEndFrameDelimiter() - 1);
+			if(getEndFrameDelimiter() < getStartFrameDelimiter())
+				return false;
+		}
 		return true;
-	}
+	} //setParsingParametersAutomatically
+	
+	/**
+	* Resets the parsing parameters to default
+	* 
+	*/
+	private void resetParsingParameters() {
+		setStartDatapointDelimiter(0);
+		setStartFrameDelimiter(0);
+	} //resetParsingParameters
+
+	private boolean isDouble(String s){
+		try{
+			Double.parseDouble(s);
+			return true;
+		}
+		catch(Exception e){
+		return false;
+		}
+	} //isDouble
+	
+	private int getNumDatapoints(){
+		return getEndDatapointDelimiter() - getStartDatapointDelimiter() + 1; // Inclusive
+	} //getNumDatapoints
+
+	public void printParsingParameters() {
+		System.out.println("Frame delimiter: " + getFrameDelimiter());
+		System.out.println("Datapoint delimiter: " + getDatapointDelimiter());
+		System.out.println("Start frame parsing: " + getStartFrameDelimiter());
+		System.out.println("End frame parsing: " + getEndFrameDelimiter());
+		System.out.println("Start datapoint parsing: " + getStartDatapointDelimiter());
+		System.out.println("End datapoint parsing: " + getEndDatapointDelimiter());
+	} //printParsingParameters
 }
