f7f6d674e9f74acf0021a20c86fad821f1ff9e37
Mark <halonen.mark@gmail.com>
2016-07-15 23:24:39 -0500

    #11

--- src/net/relinc/datafileparser/application/Main.java
+++ src/net/relinc/datafileparser/application/Main.java
@@ -3,9 +3,6 @@ package net.relinc.datafileparser.application;
 import javafx.application.Application;
 import javafx.stage.Stage;
 import net.relinc.datafileparser.GUI.Home;
-import javafx.scene.Scene;
-import javafx.scene.layout.BorderPane;
-
 
 public class Main extends Application {
 	@Override
