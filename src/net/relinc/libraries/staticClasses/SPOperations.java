424c682845c974730451f539d8f548d832cf6540
markhalonen <halonen.mark@gmail.com>
2016-09-22 21:02:26 -0400

    #4 Cleanup

--- src/net/relinc/libraries/staticClasses/SPOperations.java
+++ src/net/relinc/libraries/staticClasses/SPOperations.java
@@ -111,8 +111,9 @@ public final class SPOperations {
 				e.printStackTrace();
 				return false;
 			}
+			return true;
 		}
-		return true;
+		return false; //mac
 	}
 
 	public static void writeStringToFile(String file, String path){
@@ -163,24 +164,6 @@ public final class SPOperations {
 		}
 		return text;
 	}
-
-	public static void unzip(){
-		String source = "some/compressed/file.zip";
-		String destination = "some/destination/folder";
-		String password = "password";
-
-		try {
-			ZipFile zipFile = new ZipFile(source);
-			if (zipFile.isEncrypted()) {
-				zipFile.setPassword(password);
-			}
-			zipFile.extractAll(destination);
-		} catch (ZipException e) {
-			e.printStackTrace();
-		}
-	}
-
-
 
 	public static void deleteFolder(File folder) {
 
