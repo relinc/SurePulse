f299540ecdf4ab1d980d9ac65dd7eeab984a6063
markhalonen <halonen.mark@gmail.com>
2016-10-22 14:00:51 -0400

    Set SimpleFormatter system property for output

--- src/net/relinc/libraries/staticClasses/SPLogger.java
+++ src/net/relinc/libraries/staticClasses/SPLogger.java
@@ -10,6 +10,8 @@ public final class SPLogger {
 
 	public static Logger logger = Logger.getLogger("SPLogger");
 	static{
+		System.setProperty("java.util.logging.SimpleFormatter.format", 
+	            "%1$tF %1$tT %4$s %2$s : %5$s%6$s%n");
 		try {
 			FileHandler handler = new FileHandler(SPSettings.applicationSupportDirectory + "/RELFX/Log/" + "SPLog.%u.%g.txt", 1024 * 1024, 10, true);
 			handler.setFormatter(new SimpleFormatter());
