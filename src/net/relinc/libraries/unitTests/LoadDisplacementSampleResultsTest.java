91958179639a722353ff46037c129fa00f4dbcc6
Mark Halonen <halonen.mark@gmail.com>
2017-10-31 23:54:05 -0400

    #8 Begin writing tests. [ci skip]

--- src/net/relinc/libraries/unitTests/LoadDisplacementSampleResultsTest.java
+++ src/net/relinc/libraries/unitTests/LoadDisplacementSampleResultsTest.java
@@ -1,12 +1,33 @@
 package net.relinc.libraries.unitTests;
 
+import static org.junit.Assert.*;
+
 import org.junit.Test;
+
+import net.relinc.libraries.sample.CompressionSample;
+import net.relinc.libraries.sample.LoadDisplacementSampleResults;
 
 public class LoadDisplacementSampleResultsTest extends BaseTest{
 
 	@Test
 	public void test() {
 		//fail("Not yet implemented");
+	}
+	
+	@Test
+	public void testGetTrueStrainCompression(){
+		double[] displacement = {.01, .02, .03, .04, .05};
+		CompressionSample c = new CompressionSample();
+		c.setLength(1.0);
+		LoadDisplacementSampleResults results = new LoadDisplacementSampleResults(c);
+		results.displacement = displacement;
+		double[] trueStrain = results.getTrueStrain();
+		assertArrayEquals(new double[]{.05,  .1}, trueStrain, .01);
+	}
+	
+	@Test
+	public void testGetTrueStrainTension(){
+		
 	}
 
 }
