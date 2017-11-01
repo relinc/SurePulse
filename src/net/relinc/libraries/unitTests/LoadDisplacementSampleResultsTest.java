d905ccfc35e230e0889796cf6bc5b5673ef66025
Brent Halonen <bhalonen@gmail.com>
2017-11-01 01:24:16 -0400

    Added gut check test for true strain

--- src/net/relinc/libraries/unitTests/LoadDisplacementSampleResultsTest.java
+++ src/net/relinc/libraries/unitTests/LoadDisplacementSampleResultsTest.java
@@ -16,18 +16,24 @@ public class LoadDisplacementSampleResultsTest extends BaseTest{
 	
 	@Test
 	public void testGetTrueStrainCompression(){
-		double[] displacement = {.01, .02, .03, .04, .05};
+		double[] displacement = {.01, .02, .04, .08, .16, .32};
 		CompressionSample c = new CompressionSample();
 		c.setLength(1.0);
 		LoadDisplacementSampleResults results = new LoadDisplacementSampleResults(c);
 		results.displacement = displacement;
 		double[] trueStrain = results.getTrueStrain();
-		assertArrayEquals(new double[]{.05,  .1}, trueStrain, .01);
+		assertArrayEquals(new double[]{.01, .02, .0408, .0834, .174, 0.385 }, trueStrain, .001);
 	}
 	
 	@Test
 	public void testGetTrueStrainTension(){
-		
+		double[] displacement = {.01, .02, .04, .08, .16, .32};
+		CompressionSample c = new CompressionSample();
+		c.setLength(1.0);
+		LoadDisplacementSampleResults results = new LoadDisplacementSampleResults(c);
+		results.displacement = displacement;
+		double[] trueStrain = results.getTrueStrain();
+		assertArrayEquals(new double[]{.0099, .0198, .0392, .0769, .148, 0.278 }, trueStrain, .001);
 	}
 
 }
