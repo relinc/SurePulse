f27cf5d42900e47cb86701cd689af499a9b106fb
Mark Halonen <halonen.mark@gmail.com>
2017-11-30 01:00:37 -0500

    #22 Delete unused code and cleanup. Use Java conventions

--- src/net/relinc/libraries/unitTests/TriangularFilterTest.java
+++ src/net/relinc/libraries/unitTests/TriangularFilterTest.java
@@ -1,4 +1,5 @@
 package net.relinc.libraries.unitTests;
+
 import static org.junit.Assert.*;
 import org.junit.Test;
 
@@ -6,13 +7,13 @@ import net.relinc.libraries.staticClasses.TriangularLowPassFilter;
 
 public class TriangularFilterTest extends BaseTest {
 	@Test
-	public void runFilter(){
-		double[] testArray= new double[20];
-		for(int idx=0; idx<20;idx++)
-			testArray[idx]=Math.sin(idx/4);
-		double[] result=TriangularLowPassFilter.triangularLowPass(testArray,.2);
-		assertTrue(Math.abs(result[0])<.01);
-		assertTrue(Math.abs(result[10]-.909)<.01);
-		assertTrue(Math.abs(result[19]+.756)<.01);
+	public void runFilter() {
+		double[] testArray = new double[20];
+		for (int idx = 0; idx < 20; idx++)
+			testArray[idx] = Math.sin(idx / 4);
+		double[] result = TriangularLowPassFilter.triangularLowPass(testArray, .2);
+		assertTrue(Math.abs(result[0]) < .01);
+		assertTrue(Math.abs(result[10] - .909) < .01);
+		assertTrue(Math.abs(result[19] + .756) < .01);
 	}
 }
